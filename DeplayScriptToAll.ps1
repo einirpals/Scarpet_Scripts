3c292bfe246add572b30e5fd343a840c99f45c57
einirpals <einirpals@gmail.com>
2020-08-03 15:02:58 +0000

    Added a powershell script to deplay scripts

---
+++ DeplayScriptToAll.ps1
@@ -0,0 +1,34 @@
+# Powershell script to deplay all scripts in the script folder to Minecraft saves.
+
+$ScriptsFolder = "Scripts"
+$FullScriptsPath = [IO.Path]::Combine($PSScriptRoot, $ScriptsFolder)
+
+$MinecraftScriptsFolder = "scripts"
+$MinecraftSavesPath = [IO.Path]::Combine($env:APPDATA, ".minecraft\saves")
+
+$MinecraftSavesFolders = Get-ChildItem -Directory $MinecraftSavesPath
+$ScarpetScriptsPaths = @()
+foreach($MinecraftFolder in $MinecraftSavesFolders)
+{
+    $FullScriptPath = [IO.Path]::Combine($MinecraftSavesPath, $MinecraftFolder, $MinecraftScriptsFolder)
+    if (-not (Test-Path -LiteralPath $FullScriptPath)) {   
+        try {
+            # New-Item -Path $FullScriptPath -ItemType Directory -ErrorAction Stop | Out-Null #-Force
+        }
+        catch {
+            Write-Error -Message "Unable to create scripts directory in world '$MinecraftFolder'. Error was: $_" -ErrorAction Stop
+        }
+        #"Successfully created scripts directory in world '$MinecraftFolder'."
+        #$ScarpetScriptsPaths += $FullScriptPath
+    
+    }
+    else {
+        "Script directory already existed in world '$MinecraftFolder'."
+        $ScarpetScriptsPaths += $FullScriptPath
+    }
+}
+
+foreach($ScarpetScriptsPath in $ScarpetScriptsPaths)
+{
+    Get-ChildItem -Path $FullScriptsPath -Recurse -File | Copy-Item -Destination $ScarpetScriptsPath
+}
